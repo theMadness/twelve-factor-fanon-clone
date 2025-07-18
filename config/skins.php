e7b050310b32c5eaf8a8b376587b550b8df54b41
Madness <sirrandomage@gmail.com>
2025-07-18 05:13:32 -0700

    1.44 tentative release

--- config/skins.php
+++ config/skins.php
@@ -10,6 +10,9 @@ wfLoadSkin('MinervaNeue', __DIR__ . '/../vendor/mediawiki/minerva-neue-skin/skin
 wfLoadSkin('MonoBook', __DIR__ . '/../vendor/mediawiki/mono-book-skin/skin.json');
 wfLoadSkin('Timeless', __DIR__ . '/../vendor/mediawiki/timeless-skin/skin.json');
 wfLoadSkin('Vector', __DIR__ . '/../vendor/mediawiki/vector-skin/skin.json');
+$wgDefaultUserOptions['vector-theme'] = 'os';
+$wgVectorNightMode["logged_out"] = true;
+$wgVectorNightMode["beta"] = false;
 
 // Watch and unwatch as a star icon rather than a link (for Vector skin only).
 $wgVectorUseIconWatch = true;
